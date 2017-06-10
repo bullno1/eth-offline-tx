b92d74600d22992ea9e809e087948f34df379364
Bach Le <bach@bullno1.com>
2017-06-10 14:20:49 +0800

    Minor display tweaks

--- sign-tx.js
+++ sign-tx.js
@@ -1,5 +1,9 @@
 function sign_tx(tx) {
+	console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+	console.log("ATTENTION! Please verify the transaction before signing:");
 	console.log(JSON.stringify(tx, null, 2));
+	console.log("Value in ETH: " + web3.fromWei(tx.value, 'ether'));
+	console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 	personal.unlockAccount(tx.from);
 	return eth.signTransaction(tx).raw;
 }
